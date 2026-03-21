e228593ebfc8d5116ef3eae41b3e0057d0ac6329
yagnik529 <yagnikpansuriya603@gmail.com>
2026-03-21 12:59:05 +0530

    change in api js

--- frontend/src/store/Api.js
+++ frontend/src/store/Api.js
@@ -1,9 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-const path = location.hostname;
-if(path === 'localhost'){
+let path = location.hostname;
+if (path === 'localhost') {
   path = 'http://localhost:5000/api/';
-}
-else{
+} else {
   path = 'https://kidrooapi.vercel.app/api/';
 }
 const BASE_URL = import.meta.env.VITE_API_URL || path;
